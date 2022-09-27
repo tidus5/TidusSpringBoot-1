a2f20cf11c8ba9dbce47a2103c8d4f1891933e0d
dongsong.tao <dongsong.tao@happyelements.com>
2022-09-27 17:49:11 +0800

    add jdbc  和  mysql driver

---
+++ src/main/java/com/tidus/sb/jdbc/SpringBootJdbcController.java
@@ -0,0 +1,33 @@
+package com.tidus.sb.jdbc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * https://www.cainiaojc.com/springboot/springboot-jdbc-example.html
+ * https://www.cnblogs.com/throwable/p/13326290.html
+ * https://www.cnblogs.com/fishpro/p/spring-boot-study-jdbc.html
+ */
+@RestController
+public class SpringBootJdbcController {
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @RequestMapping("/insert")
+    public String index(){
+        jdbc.execute("insert into user(name,email)values('nhooo','java@(cainiaojc.com)')");
+        return"data inserted Successfully";
+    }
+
+    @GetMapping("/users")
+    public Object   getUsers(){
+        List<Map<String,Object>> list=jdbc.queryForList("select * from user limit 20");
+        return  list;
+    }
+}
